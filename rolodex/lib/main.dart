c72cc868d024c8e752cf72ae0fdb2de14f81d445
tolumide-ng <tolumideshopein@gmail.com>
2026-05-25 05:27:40 +0200

    Introduces slivers

--- rolodex/lib/main.dart
+++ rolodex/lib/main.dart
@@ -1,20 +1,27 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:rolodex/data/contact_group.dart';
+import 'package:rolodex/screens/adaptive_layout.dart';
+
+final contactGroupsModel = ContactGroupsModel();
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RolodexApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class RolodexApp extends StatelessWidget {
+  const RolodexApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const CupertinoApp(
+      title: 'Rolodex',
+      theme: CupertinoThemeData(
+        barBackgroundColor: CupertinoDynamicColor.withBrightness(
+          color: Color(0xFFF9F9F9),
+          darkColor: Color(0xFF1D1D1D),
         ),
       ),
+      home: AdaptiveLayout(),
     );
   }
 }
---
+++ rolodex/lib/screens/contacts.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/cupertino.dart';
+import 'package:rolodex/data/contact.dart';
+import 'package:rolodex/data/contact_group.dart';
+import 'package:rolodex/main.dart';
+
+class ContactListsPage extends StatelessWidget {
+  const ContactListsPage({
+    super.key,
+    required this.listId,
+    required this.automaticallyImplyLeading,
+  });
+
+  final int listId;
+  final bool automaticallyImplyLeading;
+
+  @override
+  Widget build(BuildContext context) {
+    return _ContactListView(
+      listId: listId,
+      automaticallyImplyLeading: automaticallyImplyLeading,
+    );
+  }
+}
+
+class _ContactListView extends StatelessWidget {
+  final int listId;
+  final bool automaticallyImplyLeading;
+
+  const _ContactListView({
+    required this.listId,
+    required this.automaticallyImplyLeading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: ValueListenableBuilder<List<ContactGroup>>(
+        valueListenable: contactGroupsModel.listsNotifier,
+        builder: (context, contactGroups, child) {
+          final contactList = contactGroupsModel.findContactList(listId);
+          final contacts = contactList.alphabetizedContacts;
+
+          return CustomScrollView(
+            slivers: [
+              CupertinoSliverNavigationBar.search(
+                largeTitle: Text(contactList.title),
+                searchField: const CupertinoSearchTextField(
+                  suffixIcon: Icon(CupertinoIcons.mic_fill),
+                  suffixMode: OverlayVisibilityMode.always,
+                ),
+              ),
+              SliverList.list(
+                children: [
+                  const SizedBox(height: 20),
+                  ...contacts.keys.map(
+                    (initial) => ContactListSection(
+                      lastInitial: initial,
+                      contacts: contacts[initial]!,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ContactListSection extends StatelessWidget {
+  const ContactListSection({
+    super.key,
+    required this.lastInitial,
+    required this.contacts,
+  });
+
+  final String lastInitial;
+  final List<Contact> contacts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+      child: Column(
+        children: [
+          const SizedBox(height: 15),
+          Align(
+            alignment: AlignmentDirectional.bottomStart,
+            child: Text(
+              lastInitial,
+              style: const TextStyle(
+                color: CupertinoColors.systemGrey,
+                fontSize: 15,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          CupertinoListSection(
+            backgroundColor: CupertinoColors.systemBackground,
+            dividerMargin: 0,
+            additionalDividerMargin: 0,
+            topMargin: 4,
+            children: [
+              for (final Contact contact in contacts)
+                CupertinoListTile(
+                  title: Text('${contact.firstName} ${contact.lastName}'),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
